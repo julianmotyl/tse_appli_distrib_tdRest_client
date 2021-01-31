05d4b90c9d148876e7e250196727b7661fdb3dab
Julian Motyl <julian97.motyl@gmail.com>
2021-01-31 23:10:26 +0100

    Ajout des GETs pour les equipes et les joueurs

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const axios = require('axios');
 
-axios.get('http://localhost:8080/employees')
+axios.get('http://localhost:8080/Joueurs')
   .then(function (response) {
     // handle success
-    console.log(response.data.length + ' employees found :');
+    console.log(response.data.length + ' Joueur(s) found :');
 
     response.data.forEach(element => {
         
-        console.log(' - ' + element.name + ', ' + element.role);
+        console.log(' - ' + element.name + ', ' + element.role + ',' + element.team);
+    });
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
+  axios.get('http://localhost:8080/Equipes')
+  .then(function (response) {
+    // handle success
+    console.log(response.data.length + ' Equipe(s) found :');
+
+    response.data.forEach(element => {
+        
+        console.log(' - ' + element.name + ', ' + element.competition);
     });
   })
   .catch(function (error) {
